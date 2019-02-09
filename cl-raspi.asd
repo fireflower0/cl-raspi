71061ed234e605f3d60cf16262397c6513538a78
fireflower0 <fireflower0@hotmail.co.jp>
2019-02-09 22:39:07 +0900

    #1 first commit

---
+++ cl-raspi.asd
@@ -0,0 +1,16 @@
+(defsystem "cl-raspi"
+    :class :package-inferred-system
+    :version "0.1.0"
+    :author "fireflower0"
+    :license "MIT"
+    :depends-on ("cffi"
+                 "cl-raspi/lib-wiring-pi"))
+
+(defsystem "cl-raspi/examples"
+    :class :package-inferred-system
+    :version "0.1.0"
+    :author "fireflower0"
+    :license "MIT"
+    :depends-on ("cffi")
+    :pathname "examples"
+    :components ((:file "00-blink")))
---
+++ lib-wiring-pi.lisp
@@ -0,0 +1,34 @@
+(defpackage :cl-raspi/lib-wiring-pi
+  (:use :cl
+        :cffi)
+  (:export ))
+(in-package :cl-raspi/lib-wiring-pi)
+
+(define-foreign-library lib-wiring-pi
+  (:unix "libwiringPi.so"))
+
+;;; Constant
+
+;; GPIO11(pin 23)
+(defconstant +gpio-11+ 11)
+
+;; GPIO Mode
+(defconstant +gpio-input-mode+ 0)
+(defconstant +gpio-output-mode+ 1)
+
+;;; API
+
+;; Init wiringPi
+(defcfun ("wiringPiSetupGpio" wiringpi-setup-gpio) :int)
+
+;; GPIO pin mode setting
+(defcfun ("pinMode" pin-mode) :void
+  (pin :int) (mode :int))
+
+;; Output control of GPIO pin
+(defcfun ("digitalWrite" digital-write) :void
+  (pin :int) (value :int))
+
+;; Delay (millisecond)
+(defcfun ("delay" delay) :void
+  (howlong :uint))
