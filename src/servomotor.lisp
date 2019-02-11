2c688cf8db0bfd3a4f542892d10e7c6156652cb9
fireflower0 <fireflower0@hotmail.co.jp>
2019-02-11 21:44:41 +0900

    Fix a servomotor program

--- src/servomotor.lisp
+++ src/servomotor.lisp
@@ -6,13 +6,14 @@
 
 (defconstant +pin+ 12)
 
-(defparameter *set-degree* 74)
+(defparameter *move* 0)
 
 (defun main ()
-  (let ((move-deg (* (+ (/ (* 4.75 *set-degree*) 90) 7.25) (/ 1024 100))))
-    (wiringpi-setup-gpio)
-    (pin-mode +pin+ 2)
-    (pwm-set-mode 0)
-    (pwm-set-range 1024)
-    (pwm-set-clock 192)
-    (pwm-write +pin+ move-deg)))
+  (wiringpi-setup-gpio)
+  (pin-mode +pin+ 2)
+  (pwm-set-mode 0)
+  (pwm-set-range 1024)
+  (pwm-set-clock 375)
+  (loop
+    (setf *move* (read))
+    (pwm-write +pin+ *move*)))
