7e4f605bb620d3ef20f96087e2f62536dcb4274a
fireflower0 <fireflower0@hotmail.co.jp>
2019-03-27 21:12:34 +0900

    Add date and time display

--- src/simple-temperature.lisp
+++ src/simple-temperature.lisp
@@ -18,11 +18,16 @@
 (defun get-data (fd)
   (* (byte-swap (wiringpi-i2c-read-reg16 fd #X00)) 0.0078))
 
+(defun get-date ()
+  (multiple-value-bind (second minute hour date month)
+      (decode-universal-time (get-universal-time))
+    (format nil "~A/~A ~A:~A:~A" month date hour minute second)))
+
 (defun main ()
   (let ((adt7410-fd (wiringpi-i2c-setup +i2c-addr-adt7410+)))
     (bo1602dgrnjb-init)
     (wiringpi-i2c-write-reg8 adt7410-fd #X03 #X80)
     (loop
-      (bo1602dgrnjb-text 1 "Temperature:")
-      (bo1602dgrnjb-text 2 (format nil "~,2f" (get-data adt7410-fd)))
+      (bo1602dgrnjb-text 1 (get-date))
+      (bo1602dgrnjb-text 2 (format nil "temp:~,2f" (get-data adt7410-fd)))
       (delay 1000))))
