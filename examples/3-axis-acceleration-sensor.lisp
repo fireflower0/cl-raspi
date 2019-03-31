2dfa061b4152a7721f56928fc13b7f642e78d4c3
fireflower0 <fireflower0@hotmail.co.jp>
2019-03-31 11:01:31 +0900

    #58 Fixed a 3-axis-acceleration-sensor program

--- examples/3-axis-acceleration-sensor.lisp
+++ examples/3-axis-acceleration-sensor.lisp
@@ -5,25 +5,20 @@
   (:export :main))
 (in-package :cl-raspi/examples/3-axis-acceleration-sensor)
 
-(defconstant +spi-cs+ 0)                ; Select target SPI device
-(defconstant +spi-speed+ 100000)        ; SPI communication speed
-
-(defconstant +out-x-l+ #X28)            ; OUT_X Low
-(defconstant +out-x-h+ #X29)            ; OUT_X High
-(defconstant +out-y-l+ #X2A)            ; OUT_Y Low
-(defconstant +out-y-h+ #X2B)            ; OUT_Y High
-(defconstant +out-z-l+ #X2C)            ; OUT_Z Low
-(defconstant +out-z-h+ #X2D)            ; OUT_Z High
-
-(defconstant +who-am-i+  #X0F)          ; WHO_AM_I
-(defconstant +ctrl-reg1+ #X20)          ; CTRL_REG1
-(defconstant +ctrl-reg4+ #X23)          ; CTRL_REG4
-
-(defconstant +read+  #X80)              ; Read
-(defconstant +write+ #X3F)              ; Write
-
-(defconstant +high+  1)
-(defconstant +low+   0)
+(defconstant +spi-cs+     0)
+(defconstant +spi-speed+  100000)
+(defconstant +out-x-l+    #X28)
+(defconstant +out-x-h+    #X29)
+(defconstant +out-y-l+    #X2A)
+(defconstant +out-y-h+    #X2B)
+(defconstant +out-z-l+    #X2C)
+(defconstant +out-z-h+    #X2D)
+(defconstant +who-am-i+   #X0F)
+(defconstant +lis3dh+     #X33)
+(defconstant +ctrl-reg1+  #X20)
+(defconstant +xyz-enable+ #X7F)
+(defconstant +read+       #X80)
+(defconstant +write+      #X3F)
 
 (defun spi-data-rw (channel data &optional (len (length data)))
   (let ((mp (cffi:foreign-alloc :unsigned-char :count len :initial-contents data)))
@@ -34,9 +29,8 @@
       rval)))
 
 (defun spi-read (read-addr)
-  (let (outdat out)
-    (setf outdat (list (logior read-addr +read+) #X00))
-    (setf out (spi-data-rw +spi-cs+ outdat))
+  (let* ((outdat (list (logior read-addr +read+) #X00))
+         (out    (spi-data-rw +spi-cs+ outdat)))
     (nth 1 out)))
 
 (defun spi-write (write-addr data)
@@ -50,22 +44,19 @@
     (ash dat -4)))
 
 (defun get-acceleration (low-reg high-reg)
-  (let ((lb  (spi-read low-reg))
-        (hb  (spi-read high-reg)))
+  (let ((lb (spi-read low-reg))
+        (hb (spi-read high-reg)))
     (conv-two-byte hb lb)))
 
 (defun main ()
-  (let (lb hb x y z)
-    (wiringpi-spi-setup +spi-cs+ +spi-speed+)
-
-    (if (equal (spi-read +who-am-i+) #X33)
-        (format t "I AM LIS3DH~%")
-        (return-from main nil))
-    (spi-write +ctrl-reg1+ #X77)
-
-    (loop
-       (format t "x=~6d y=~6d z=~6d~%"
-               (get-acceleration +out-x-l+ +out-x-h+)
-               (get-acceleration +out-y-l+ +out-y-h+)
-               (get-acceleration +out-z-l+ +out-z-h+))
-       (delay 500))))
+  (wiringpi-spi-setup +spi-cs+ +spi-speed+)
+  (if (equal (spi-read +who-am-i+) +lis3dh+)
+      (format t "I AM LIS3DH~%")
+      (return-from main nil))
+  (spi-write +ctrl-reg1+ +xyz-enable+)
+  (loop
+    (format t "x=~6d y=~6d z=~6d~%"
+            (get-acceleration +out-x-l+ +out-x-h+)
+            (get-acceleration +out-y-l+ +out-y-h+)
+            (get-acceleration +out-z-l+ +out-z-h+))
+    (delay 500)))
