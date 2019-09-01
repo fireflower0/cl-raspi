0dee4b498ca0b16ca285aa60fe2d1294f70ddf21
fireflower0 <fireflower0@hotmail.co.jp>
2019-09-01 15:07:30 +0900

    Fixed a color led

--- examples/color.lisp
+++ examples/color.lisp
@@ -4,9 +4,11 @@
   (:export :main))
 (in-package :cl-raspi/examples/color)
 
-(defconstant +green-pin+ 18)
-(defconstant +blue-pin+  23)
-(defconstant +red-pin+   24)
+(defconstant +green-pin+ 27)
+(defconstant +blue-pin+ 22)
+(defconstant +red-pin+ 17)
+(defconstant +initial-value+ 0)
+(defconstant +pwm-range+ 100)
 
 (defun main ()
   ;; GPIO初期化
@@ -15,11 +17,14 @@
   (pin-mode +green-pin+ +output+)
   (pin-mode +blue-pin+  +output+)
   (pin-mode +red-pin+   +output+)
-  ;; PWM出力設定
-  (soft-pwm-create +green-pin+ 0 100)
-  (soft-pwm-create +blue-pin+  0 100)
-  (soft-pwm-create +red-pin+   0 100)
-  ;; PWMで各端子に出力
-  (soft-pwm-write +green-pin+ 30)
-  (soft-pwm-write +blue-pin+  50)
-  (soft-pwm-write +red-pin+  100))
+  (loop
+     ;; PWM出力設定
+     (soft-pwm-create +green-pin+ +initial-value+ +pwm-range+)
+     (soft-pwm-create +blue-pin+  +initial-value+ +pwm-range+)
+     (soft-pwm-create +red-pin+   +initial-value+ +pwm-range+)
+     ;; PWMで各端子に出力
+     (soft-pwm-write +green-pin+ (random 100))
+     (soft-pwm-write +blue-pin+  (random 100))
+     (soft-pwm-write +red-pin+   (random 100))
+     ;; 待機
+     (delay 500)))
